5a4e65bcbed3353f009b2f60c725fda79db023ed
Marat Shagiev <m_js@bk.ru>
2016-08-21 02:20:03 +0300

    hostscript, function repl()
    fixed a bug:
    
    when a regular expression pattern is a sign begin or end of the string (anchors),
     the loop becomes infinite
    
     and now the anchors is search well

--- com.wk.ai_regexRepl/jsx/hostscript.jsx
+++ com.wk.ai_regexRepl/jsx/hostscript.jsx
@@ -12,20 +12,28 @@
  * @param {String} replacer - replacer string
  * */
 function repl (regStr, replacer) {
+
   var count = 0;
   if (selection.length != 1) {
     alert ('Select the text frame by Selection Tool, Direct Selection Tool or Group Selection Tool');
     return -1;
   }
   var txtFrame = selection[0],
-      reg      = new RegExp (regStr, 'gi'),
+      reg      = new RegExp (regStr, 'gmi'),
       result;
+  var resIndex = 0;
 
   while (result = reg.exec (txtFrame.contents)) {
+    // when a regular expression pattern is a sign begin or end of the string (anchors),
+    // the loop becomes infinite maybe because in this case the result[0].length == 0
+    if (result[0].length == 0) {
+      reg.lastIndex += 2;
+    }
     try {
       var aCon      = txtFrame.characters[result.index];
       aCon.length   = result[0].length;
       aCon.contents = aCon.contents.replace (reg, replacer);
+      // aCon.contents = replacer;
       count++;
     } catch (e) {
     }
